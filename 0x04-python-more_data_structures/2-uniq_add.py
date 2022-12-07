cce1b23f496b7cc545903481a8770968cb943e84
Lilianchukwubude <lilianchukwubude@gmail.com>
2022-12-07 04:26:15 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
- (3 sloc)  75 Bytes
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    new = set(my_list)
+    res = 0
+    for i in new:
+        res += i
+    return res
