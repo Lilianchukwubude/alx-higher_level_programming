316aa53c52a3740d9a477acf1e138d41b13e91c1
Lilianchukwubude <lilianchukwubude@gmail.com>
2022-12-07 03:59:46 -0800

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x * number, my_list)))
 
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
