f4f198e519689b25aa1b7cfee6e40e9d6f169def
Lilianchukwubude <Lilianchukwubude@gmail.com>
2023-02-15 07:06:34 -0800

    3sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates a table force_name
+CREATE table IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
+
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- creates a table force_name
+CREATE table IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
+
