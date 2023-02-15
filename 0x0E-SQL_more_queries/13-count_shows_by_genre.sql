31918894710f3fa9f707ec9a40a4a08d6dd40f08
Lilianchukwubude <Lilianchukwubude@gmail.com>
2023-02-15 07:13:33 -0800

    4sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
--- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-       FROM tv_show_genres
-       INNER JOIN tv_genres
-       ON tv_genres.id = tv_show_genres.genre_id
-       GROUP BY tv_genres.name
-       ORDER BY number_shows DESC;
-
+-- Script that lists genres and number of shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- Query to join tables
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
