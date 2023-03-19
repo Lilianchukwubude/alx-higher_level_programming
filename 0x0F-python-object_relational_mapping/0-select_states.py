c4cb3ce54a845f31d5cace0187ae53a71e105be5
Lilianchukwubude <lilianchukwubude@gmail.com>
2023-03-19 11:03:08 -0700

    mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-
-
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
-'''
-a script that lists all states
-from the database
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
-    cursor.close()
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states ORDER BY states.id")
+    res = cr.fetchall()
+    for rows in res:
+        print(rows)
+    cr.close()
     db.close()
-
