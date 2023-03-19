b6122be2f96ceeb0cb2330feff7063aa606ce103
Lilianchukwubude <lilianchukwubude@gmail.com>
2023-03-19 11:14:09 -0700

    mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-
-
+"""script that lists all states with a name
+starting with N from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
-'''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cr = db.cursor()
+    myQuery = " ".join([
+        "SELECT * FROM states",
+        "WHERE name LIKE BINARY 'N%'",
+        "ORDER BY id ASC"])
+    cr.execute(myQuery)
+    res = cr.fetchall()
+    for rows in res:
+        print(rows)
+    cr.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-'''
-    Defines classes for tables
-'''
+"""Module that containes the class definition of City and instance Base"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    '''
-        Creates table for cities
-    '''
-    __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False)
+    """City class that inherits from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="cities")
